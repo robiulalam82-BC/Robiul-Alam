a6a87aea9bfbdf15824c1c853e6dd661086d6205
robiulalam6121117 <60680984+robiulalam6121117@users.noreply.github.com>
2020-02-25 19:11:13 -0500

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,27 @@
+// a1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+int input(istream& in = cin)
+{
+    string x;
+    in >> x;
+    return 0;
+}
+int main()
+{
+    string mylist;
+    int spotify[4][100];
+
+myfunction: {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 100; j++)
+        {
+            spotify[i][j] = input();
+        }
+    }
+    }
+}
